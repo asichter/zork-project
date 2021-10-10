74faa20b8298ab5bb16e730555b64f9e066e8644
Raviteja Vajja <70241397+rvajja46@users.noreply.github.com>
2021-10-10 17:11:43 -0400

    Delete project directory

--- project/Attack.cpp
+++
@@ -1,7 +0,0 @@
-#include "Attack.h"
-
-Attack::Attack(std::vector<Condition*> _conditions, std::vector<std::string> _prints, std::vector<Action*> _actions) {
-    conditions = _conditions;
-    prints = _prints;
-    _actions = _actions;
-}
